5625ee1d970f49e163fefb689bf78cdc0464a7b3
faria <faria@DESKTOP-TLUUL68>
2023-06-30 21:22:34 -0300

    Arreglando los if

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -9,8 +9,10 @@ const createUser = async(req,res)=>{
         const newUser = await userService.createUser(req.body);
         if(newUser){
             res.json(newUser);
+        }else{
+
+            res.status(400).json({message:'Error when creating User.'});
         }
-        res.status(400).json({message:'Error when creating User.'});
     } catch(err) {
         res.status(400).json({
             action:'Create user.' ,
@@ -24,11 +26,13 @@ const updateUser = async(req,res)=>{
        const userUpdate = await userService.updateUser(req.params.userId, req.body);
        if(userUpdate){
            res.json(userUpdate);
+       }else{
+
+           res.status(404).json({
+                     action:'Update User.' ,
+                     error: 'User Not Found'
+            })
        }
-       res.status(404).json({
-                 action:'Update User.' ,
-                 error: 'User Not Found'
-        })
     
     } catch(err) {
         res.status(400).json({
@@ -45,11 +49,13 @@ const deleteUser = async(req, res)=>{
          const userDelet= await userService.deleteUser(req.params.userId);
         if(userDelet){
             res.json(userDelet);
+        }else{
+
+            res.status(404).json({
+                      action:'Delete User.' ,
+                      error: 'User Not Found'
+             })
         }
-        res.status(404).json({
-                  action:'Delete User.' ,
-                  error: 'User Not Found'
-         })
     } catch(err) {
         res.status(400).json({
             action:'Delete User.' ,
@@ -67,10 +73,13 @@ const findUserByPK = async(req, res)=>{
         if(user){
             res.json(user);
            }
-           res.status(404).json({
-            action:'Find User by PK.',
-            error: `Not Find User by PK ${req.params.userId}.`
-        })
+           else{
+               res.status(404).json({
+                action:'Find User by PK.',
+                error: `Not Find User by PK ${req.params.userId}.`
+            })
+
+           }
      } catch(err) {
          res.status(400).json({
              action: 'Find User by PK',
@@ -84,12 +93,14 @@ const getAllUser = async(req,res)=>{
        const users = await userService.getAllUser();
        if(users.length !== 0){
         res.json(users);
-       } 
+       } else{
 
-       res.status(404).json({
-        action:'Get All Users',
-        error: 'Not found Users.'
-    });
+           res.status(404).json({
+            action:'Get All Users',
+            error: 'Not found Users.'
+        });
+       }
+
        
     }catch(err){
         res.status(400).json({
